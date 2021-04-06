34d4019c1110cb7cbb10a9bc57ab36c37cb28de6
Renato Carauta Ribeiro <rcarauta@unb.br>
2021-04-06 20:59:18 -0300

    proejct hr-apigatway-zuul created

---
+++ hr-api-gatway-zuul/src/test/java/com/gatway/hrapigatwayzull/HrApiGatwayZuulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gatway.hrapigatwayzull;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrApiGatwayZuulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
