0ce1e76aca2a2deaede43f685258e81c9e5f5995
Renato Carauta Ribeiro <rcarauta@unb.br>
2021-04-07 19:11:51 -0300

    Creation autentication jwt

--- hr-oauth/src/main/java/com/oauth/hroauth/services/UserService.java
+++ hr-oauth/src/main/java/com/oauth/hroauth/services/UserService.java
@@ -1,6 +1,9 @@
 package com.oauth.hroauth.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import com.oauth.hroauth.entitites.User;
@@ -9,7 +12,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 @Service
-public class UserService {
+public class UserService implements UserDetailsService {
 	
 	private static Logger logger = LoggerFactory.getLogger(UserService.class);
 
@@ -24,6 +27,11 @@ public class UserService {
 		}
 		logger.info("Email found: "+email);
 		return user;
+	}
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		return findBtEmail(username);
 	}
 	
 }
