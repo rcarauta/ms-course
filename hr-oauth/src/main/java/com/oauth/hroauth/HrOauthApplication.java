615335449ef50e1dc92457bccf63dace68dc7d6c
Renato Carauta Ribeiro <rcarauta@unb.br>
2021-04-07 21:27:31 -0300

    Security configs to config-server

--- hr-oauth/src/main/java/com/oauth/hroauth/HrOauthApplication.java
+++ hr-oauth/src/main/java/com/oauth/hroauth/HrOauthApplication.java
@@ -1,5 +1,7 @@
 package com.oauth.hroauth;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
@@ -8,10 +10,25 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @EnableFeignClients
 @EnableEurekaClient
 @SpringBootApplication
-public class HrOauthApplication {
+public class HrOauthApplication implements CommandLineRunner {
+	
+	@Value("${oauth.client.name}")
+	private String clientName;
+	
+	@Value("${oauth.client.secret}")
+	private String clientSecret;
+	
 
 	public static void main(String[] args) {
 		SpringApplication.run(HrOauthApplication.class, args);
 	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Client name = "+clientName);
+		System.out.println("Client secret = "+clientSecret);
+	}
+	
+	
 
 }
